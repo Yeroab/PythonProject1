b1858e493d008f8895cf97bccd89fd035b6a1a7c
Yeroab Ketema Samuel <yeroabs@gustavus.edu>
2026-01-19 12:55:53 -0500

    Update GBM1.py

--- GBM1.py
+++ GBM1.py
@@ -28,7 +28,7 @@ app_mode = st.sidebar.selectbox("Select Page",
 
 # --- PAGE 1: MAIN DIAGNOSIS ---
 if app_mode == "Upload your own omics data":
-    st.title(" GBM_Omics Analysis Page for User Data")
+    st.title(" GBM_Omics User Driven Interface")
     if diag:
         model = diag['model']
         all_features = diag['features']
